e5ea587f443806b6504217efc05f14dafa8376dc
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2020-11-07 02:30:01 -0800

    feat: add tests to docker file

--- backend/api/app/core/config.py
+++ backend/api/app/core/config.py
@@ -5,9 +5,14 @@ config = Config(".env")
 
 
 class AppConfig:
-    
+    # AWS account config
     AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID", cast=Secret, default=None)
     AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY", cast=Secret, default=None)
     AWS_DEFAULT_REGION = config("AWS_DEFAULT_REGION", cast=str, default="")
-
+    
+    # S3 storage config
     S3_BUCKET_NAME = config("S3_BUCKET_NAME", cast=str, default=None)
+
+    # Redis cache config
+    REDIS_HOST = config("REDIS_HOST", cast=str, default="127.0.0.1")
+    REDIS_PORT = config("REDIS_PORT", cast=int, default=6379)
